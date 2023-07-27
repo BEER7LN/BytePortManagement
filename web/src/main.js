9cc6b02507ae069c8eb6104d4880a7d3f303ee52
Your Name <3338182059@qq.com>
2023-07-27 17:12:05 +0800

    加入基本依赖
    axios +router +less+pinia+Ant

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+import './assets/style/custom.less'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+const pinia =createPinia()
+createApp(App)
+    .use(Antd) 
+		.use(pinia)
+		.use(router)
+    .mount('#app')
---
+++ web/src/store/index.js
@@ -0,0 +1,5 @@
+import { defineStore } from 'pinia'
+
+export const useXXXStore = defineStore('XXX', {
+	// .....配置
+})

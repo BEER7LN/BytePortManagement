32edced2db3d3d9c1e336c117790a17767e9d123
BEER7LN <961659634@qq.com>
2024-11-18 22:45:22 +0800

    移动并修改

---
+++ web/src/pages/Home/RunInterface/login.tsx
@@ -0,0 +1,18 @@
+import LoginForm from './LoginForm';
+import styles from './index.module.scss';
+const aa  = new type(arguments);
+const arr = [1, 2, 3];
+const obj = {a: 1, b: 2};
+const obj2 = {a: 1, b: 2};
+const obj3 = {a: 1, b: 2};
+const obj4 = {a: 1, b: 2};
+const obj5 = {a: 1, b: 2};
+const Login: React.FC = () => {
+	return (
+		<div className={styles.container}>
+			<LoginForm />
+		</div>
+	)
+}
+
+export default Login
